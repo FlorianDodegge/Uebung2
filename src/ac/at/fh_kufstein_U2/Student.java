09f32ed1eed0945949c93f331007dedc14caa3b7
MacBook <MacBook@karstadsmacbook.fritz.box>
2014-05-03 12:18:25 +0200

    new commit

---
+++ src/ac/at/fh_kufstein_U2/Student.java
@@ -0,0 +1,28 @@
+
+package ac.at.fh_kufstein_U2;
+
+public class Student extends Kind {
+    
+    long matrikelnummer;
+    String studiengang;
+
+    public Student(long matrikelnummer, String studiengang, String vorname, String nachname, int alter) {
+        super(vorname, nachname, alter);
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    @Override
+    public String Status() {
+        return "Es handelt sich um einen Studenten mit dem Namen: " + getVorname() + " " + getNachname() + " und ist " + getAlter() + " Jahre alt, hat die Matrikelnummer: " + matrikelnummer + " und studiert " + studiengang;
+    }
+    
+    public static void main(String[] args) {
+        
+        Kind k = new Kind("Florian","Dodegge",26);
+        System.out.println(k.Status());
+        
+        Student s = new Student(1310653831,"WEB","Florian","Dodegge",26);
+        System.out.println(s.Status());
+    }
+}
