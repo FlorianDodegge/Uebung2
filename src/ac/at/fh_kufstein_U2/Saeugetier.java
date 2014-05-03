876415ab2c0c17083d2aead13d1b1f38b7aaa586
MacBook <MacBook@karstadsmacbook.fritz.box>
2014-05-03 11:05:40 +0200

    new commit

---
+++ src/ac/at/fh_kufstein_U2/Saeugetier.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ac.at.fh_kufstein_U2;
+
+/**
+ *
+ * @author MacBook
+ */
+public class Saeugetier {
+    
+}
